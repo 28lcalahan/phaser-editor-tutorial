691b562662885c0be47febf1ff604b938be0c932
Patrick Calahan <github@pcal.net>
2024-09-04 16:54:46 -0700

    checkpoint

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,7 @@ window.addEventListener('load', function () {
 			mode: Phaser.Scale.ScaleModes.FIT,
 			autoCenter: Phaser.Scale.Center.CENTER_BOTH
 		},
-		scene: [Boot, Preload, PhaserTutorialMain]
+		scene: [Boot, Preload, Level, PhaserTutorialMain]
 	});
 
 	game.scene.start("Boot");
